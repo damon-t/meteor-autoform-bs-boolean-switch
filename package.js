121fa8996964da385add3714647633cfb63366a7
damon-t <git@8thsense.de>
2020-12-07 13:52:08 +0100

    update name

--- package.js
+++ package.js
@@ -1,12 +1,12 @@
 Package.describe({
-    name: 'damon-t:autoform-bs-boolean-switch',
+    name: 'odem:autoform-bs-boolean-switch',
     summary: 'A Bootstrap button switch input types for AutoForm ',
     version: '1.0.0',
     git: 'https://github.com/damon-t/meteor-autoform-bs-boolean-switch'
   });
   
   Package.onUse(function(api) {
-    api.use('templating@1.0.0');
+    api.use('templating@1.3.2');
     api.use('blaze@2.3.4');
     api.use('aldeed:autoform@7.0.0');
     api.addFiles([
